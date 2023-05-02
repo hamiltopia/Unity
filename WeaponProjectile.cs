1c49ff73be48d28a12d2654808780e9c693a9cb6
Mike Hamilton <mike@hamiltopia.com>
2023-05-02 19:54:37 -0400

    Script to be attached to a projectile

---
+++ WeaponProjectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponProjectile : MonoBehaviour
+{
+	public float speed = 100f;
+
+	public GameObject blast;
+
+	public float lifespan = 5f;
+
+	// Use this for initialization
+	void Start()
+	{
+		Destroy(gameObject, lifespan);
+	}
+
+
+	void FixedUpdate()
+	{
+		transform.position += transform.forward * Time.fixedDeltaTime * speed;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Instantiate(blast, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+
+}
